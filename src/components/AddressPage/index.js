9c8b513126e5c6225c1091872c75e0f12beb73bb
Koppolu Koushik <koushik.koppolu00@gmail.com>
2024-01-06 10:09:39 +0530

    Motherson-by-koushik

---
+++ src/components/AddressPage/index.js
@@ -0,0 +1,27 @@
+import "./index.css";
+
+const AddressPage = () => (
+  <div id="address" className="addressPageContainer">
+    <div>
+      <img
+        src="https://res.cloudinary.com/dojn2omox/image/upload/v1704400215/linkedin_p2xapl.png"
+        alt="insta"
+        className="logo-image-details"
+      />
+      <img
+        src="https://res.cloudinary.com/dojn2omox/image/upload/v1704400215/linkedin_p2xapl.png"
+        alt="linkedin"
+        className="logo-image-details"
+      />
+      <img
+        src="https://res.cloudinary.com/dojn2omox/image/upload/v1704400447/fb2_uegrug.webp"
+        alt="fb"
+        className="logo-image-details"
+      />
+    </div>
+
+    <p>Copyright © 2024. Koppolu Koushik</p>
+  </div>
+);
+
+export default AddressPage;
